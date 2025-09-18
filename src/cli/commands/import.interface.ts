1d535a25486ffee2e70a8cdda25b5737093bd0fb
Gurikov Maxim <maximgurikoff@gmail.com>
2025-09-19 00:22:00 +0500

    started task

---
+++ src/cli/commands/import.interface.ts
@@ -0,0 +1,26 @@
+import { ICommand } from './command.interface.js';
+import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+
+export class ImportCommand implements ICommand {
+  public getName(): string {
+    return '--import';
+  }
+
+  public execute(...parameters: string[]): void {
+    const [filename] = parameters;
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${err.message}`);
+    }
+  }
+}
