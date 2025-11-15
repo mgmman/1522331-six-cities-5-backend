098d0ce48196d8afcae03735cf9e2bf5e21432c5
Gurikov Maxim <maximgurikoff@gmail.com>
2025-11-15 22:41:47 +0500

    fixes

--- src/rest/rest.container.ts
+++ src/rest/rest.container.ts
@@ -5,6 +5,7 @@ import { ILogger, PinoLogger } from '../shared/libs/logger/index.js';
 import { IConfig, RestConfig, RestSchema } from '../shared/libs/config/index.js';
 import { IDatabaseClient, MongoClient } from '../shared/libs/db-client/index.js';
 import {AppExceptionFilter, IExceptionFilter} from '../shared/libs/rest/index.js';
+import { PathTransformer } from '../shared/libs/rest/path-transformer/path-transformer.js';
 
 export const restApplicationContainer: ContainerModule = new ContainerModule(
   (options: ContainerModuleLoadOptions) => {
@@ -14,5 +15,6 @@ export const restApplicationContainer: ContainerModule = new ContainerModule(
     options.bind<IConfig<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
     options.bind<IDatabaseClient>(Component.DatabaseClient).to(MongoClient).inSingletonScope();
     options.bind<IExceptionFilter>(Component.ExceptionFilter).to(AppExceptionFilter).inSingletonScope();
+    options.bind<PathTransformer>(Component.PathTransformer).to(PathTransformer).inSingletonScope();
   });
 
